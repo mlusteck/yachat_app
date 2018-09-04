5ff10eec4e9e744bdb13e2bcc1da4e4356d8faef
Mischa Lusteck <mlusteck@googlemail.com>
2018-09-04 21:37:50 +0200

    Show and post messages in room with AJAX

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,24 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
 
-  # GET /messages
-  # GET /messages.json
   def index
-    @messages = Message.all
+    @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user).order("created_at ASC")
+    if @room
+      respond_to do |format|
+        format.js   # we want to do this with AJAX
+      end
+    end
   end
 
-  # GET /messages/1
-  # GET /messages/1.json
   def show
+    @message = Message.find(params[:id])
+    if @message
+      @room = @message.room
+      respond_to do |format|
+        format.js   # we want to do this with AJAX
+      end
+    end
   end
 
   # GET /messages/new
@@ -24,14 +33,22 @@ class MessagesController < ApplicationController
   # POST /messages
   # POST /messages.json
   def create
-    @message = Message.new(message_params)
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    @message.user = current_user
 
     respond_to do |format|
       if @message.save
-        format.html { redirect_to @message, notice: 'Message was successfully created.' }
-        format.json { render :show, status: :created, location: @message }
+        format.html { redirect_to @room, notice: 'Message was created successfully.' }
+        format.json { render :show, status: :created, location: @room }
+        format.js   # we want to do this with AJAX
       else
-        format.html { render :new }
+        error_text = ""
+        @message.errors.full_messages.each do |message|
+          error_text += " " + message
+        end
+
+        format.html { redirect_to @room, alert: 'Review was not saved successfully.' + error_text }
         format.json { render json: @message.errors, status: :unprocessable_entity }
       end
     end
@@ -69,6 +86,6 @@ class MessagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def message_params
-      params.require(:message).permit(:body, :room_id, :user_id)
+      params.require(:message).permit(:body)
     end
 end
