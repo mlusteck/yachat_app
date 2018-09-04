8076edc03c090d3ee4ef7cca011e0165b6bf22b1
Mischa Lusteck <mlusteck@googlemail.com>
2018-09-04 15:45:52 +0200

    Add default room and admin flag to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_03_143423) do
+ActiveRecord::Schema.define(version: 2018_09_04_134425) do
 
   create_table "messages", force: :cascade do |t|
     t.text "body"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2018_09_03_143423) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.integer "room_id"
+    t.integer "room_id", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2018_09_03_143423) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
